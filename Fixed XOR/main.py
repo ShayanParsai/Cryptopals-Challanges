856776d821219c30925d4fa090bb885661a53c56
Shayan Parsai <70860248+ShayanParsai@users.noreply.github.com>
2021-01-17 11:51:43 +0100

    Refined code

--- Fixed XOR/main.py
+++ Fixed XOR/main.py
@@ -1,13 +1,11 @@
 import binascii
 
-x = '1c0111001f010100061a024b53535009181c'
-y = '686974207468652062756c6c277320657965'
-deHexedX = binascii.unhexlify(x)
-deHexedY = binascii.unhexlify(y)
+x = binascii.unhexlify('1c0111001f010100061a024b53535009181c')
+y = binascii.unhexlify('686974207468652062756c6c277320657965')
 
 result = bytearray()
-for i in range(len(deHexedX)):
-    result.append((deHexedX[i] ^ deHexedY[i]))
+for i in range(len(x)):
+    result.append((x[i] ^ y[i]))
 
-print(result)
+print(result.decode())
 print(binascii.hexlify(result))
