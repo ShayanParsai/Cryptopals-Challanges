00066f20819b52ba1afb03d50d99f83f0d5b716f
Shayan Parsai <70860248+ShayanParsai@users.noreply.github.com>
2021-01-17 11:56:24 +0100

    Refined Code

--- Convert hex to base64/main.py
+++ Convert hex to base64/main.py
@@ -1,7 +1,6 @@
 import base64
 
-hexString = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
-byteArray = bytearray.fromhex(hexString)
-print("This is the hexString in bytearray format : ", byteArray)
-base64Value = base64.b64encode(byteArray)
-print("This is the base 64 value : ", base64Value)
+byteArray = bytearray.fromhex('49276d206b696c6c696e6720796f757220627261696e206c6'
+                              '96b65206120706f69736f6e6f7573206d757368726f6f6d')
+print(byteArray.decode())
+print(base64.b64encode(byteArray))
