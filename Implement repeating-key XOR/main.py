db980f92614761ed598fa0475c1a0117831eaa0f
Shayan Parsai <70860248+ShayanParsai@users.noreply.github.com>
2021-01-19 13:35:36 +0100

    Update

--- Implement repeating-key XOR/main.py
+++ Implement repeating-key XOR/main.py
@@ -2,19 +2,31 @@ import binascii
 
 
 def main():
-    message = open('TextToEncrypt.txt').readlines()
-    key = 'ICE'
+    message = open('TextToEnOrDeCrypt.txt').readlines()
     stringsList = []
     for i in range(len(message)):
         stringsList.append(message[i].strip())
 
-    #DeCrypted > Crypted
+    #Pick a key, keep it secret
+    key = 'bra fråga ;)'
+
+    #use this method if you want to encrypt your text, paste the text in the ' TextToEncrypt/Decrypt.txt'
+    encrypt(message,key,stringsList)
+
+    #use this method if you want to decrypt that text, make sure to use the key that you used to encrypt the text
+    #decrypt(message,key,stringsList)
+
+    #Comment out the method that you arent using with '#'
+
+
+def encrypt(message, key, stringsList):
     for i in range(len(stringsList)):
         stringsList[i] = binascii.hexlify(encrypt_decrypt(stringsList[i], key))
     for s in stringsList:
         print(s.decode('utf-8'))
 
-    #Crypted > DeCrypted
+
+def decrypt(message, key, stringsList):
     for i in range(len(stringsList)):
         stringsList[i] = binascii.unhexlify(stringsList[i]).decode('iso-8859-15')
         stringsList[i] = encrypt_decrypt(stringsList[i], key)
